26aed908e6e4ec9d64b5dc1e9a2ab78494f46b14
Maricar Martin <115593469+maricarmartin@users.noreply.github.com>
2022-11-17 23:31:17 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Select one that is not a programming language?",
+    answer: "B. Google",
+    options: [
+      "A. HTML",
+      "B. Google",
+      "C. CSS",
+      "D. Javascript"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does API stands for in Web API?",
+    answer: "A. Application Programming Interface",
+    options: [
+      "A. Application Programming Interface",
+      "B. Animation Program Interior",
+      "C. Application Program Infrastructure",
+      "D. Animated Programming Interface"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does CSS stands for?",
+    answer: "C. Cascading Style Sheets",
+    options: [
+      "A. Cascading Sheets Style",
+      "B. Cascade Style Shift",
+      "C. Cascading Style Sheets",
+      "D. Cascade Sheets Styling"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which HTML attribute is used to define inline styles??",
+    answer: "C. style",
+    options: [
+      "A. font",
+      "B. element",
+      "C. style",
+      "D. class",
+    ]
+  },
+    {
+    numb: 5,
+    question: "In HTML, which attribute is used to specify that an input field must be filled out?",
+    answer: "C. required",
+    options: [
+      "A. form",
+      "B. validate",
+      "C. required",
+      "D. box"
+    ]
+  },
+
+];
